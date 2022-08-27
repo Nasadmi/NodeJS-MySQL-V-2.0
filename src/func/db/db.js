4dbf163f1ccde4a39edc44d16871eb99b37258fc
Nasadmi <98781576+Nasadmi@users.noreply.github.com>
2022-08-27 13:01:10 +0200

    Update db.js

--- src/func/db/db.js
+++ src/func/db/db.js
@@ -4,7 +4,7 @@ const connector = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
-        password: 'Diego@008**$',
+        password: '',
         database: 'contact'
     }
 )
